7d4f89216c6d5dc95d901870f973f7bfb44e27f8
Sidharth <sidharthdas.1995@gmail.com>
2021-06-06 09:40:21 +0530

    testing

--- JavaCoreTopics/src/test/MainClass2.java
+++ JavaCoreTopics/src/test/MainClass2.java
@@ -13,7 +13,7 @@ public class MainClass2 {
 
 		int[] arr1 = {5,8,7,12,14,3,9};
 		MainClass2.test(arr1, div, digit);
-
+//Sidharth Das
 	}
 
 	public static void test(int[] arr, int div, int digit) {
