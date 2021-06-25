e9aceca207b8b16559ef90976e2df97d54470e83
Sidharth <sidharthdas.1995@gmail.com>
2021-06-25 22:29:12 +0530

    Immutable class

---
+++ JavaCoreTopics/src/JUNE/immutable/ImmutableImplementationMainClass.java
@@ -0,0 +1,11 @@
+package JUNE.immutable;
+
+/**
+ * @author sidharthdas on 25/06/21.
+ */
+public class ImmutableImplementationMainClass {
+
+    public static void main ( String[] args ) {
+
+    }
+}
