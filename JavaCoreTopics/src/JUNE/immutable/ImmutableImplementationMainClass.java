2382ab10c729edd7a31fbb4eeeb2d5be4d5a13a8
Sidharth <sidharthdas.1995@gmail.com>
2021-06-25 22:52:21 +0530

    Immutable class

--- JavaCoreTopics/src/JUNE/immutable/ImmutableImplementationMainClass.java
+++ JavaCoreTopics/src/JUNE/immutable/ImmutableImplementationMainClass.java
@@ -1,11 +1,16 @@
 package JUNE.immutable;
 
+import override.A;
+
+import java.util.ArrayList;
+
 /**
  * @author sidharthdas on 25/06/21.
  */
 public class ImmutableImplementationMainClass {
 
     public static void main ( String[] args ) {
-
+        Student student = new Student("Sidharth", new ArrayList<>());
+        System.out.println(student.getStudName());
     }
 }
---
+++ JavaCoreTopics/src/JUNE/immutable/Student.java
@@ -0,0 +1,32 @@
+package JUNE.immutable;
+
+import override.A;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author sidharthdas on 25/06/21.
+ */
+public final class Student {
+
+    private final String studName;
+    private final ArrayList<String> address;
+
+    public Student ( String studName, ArrayList< String > address ) {
+        this.studName = studName;
+        List<String> address1 = new ArrayList<>();
+        address.forEach(x ->{
+            address1.add(x);
+        });
+        this.address = (ArrayList< String >) address1;
+    }
+
+    public String getStudName () {
+        return studName;
+    }
+
+    public ArrayList< String > getAddress () {
+        return (ArrayList< String >) address.clone();
+    }
+}
