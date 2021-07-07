3516cbeb7ca8dff7494a85610cc67b3fcd68135d
Sidharth <sidharthdas.1995@gmail.com>
2021-07-08 00:31:02 +0530

    leetcode

--- JavaCoreTopics/src/leetcode/LeetCodeSortestElement.java
+++ JavaCoreTopics/src/leetcode/LeetCodeSortestElement.java
@@ -1,9 +1,6 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author sidharthdas on 07/07/21.
@@ -14,15 +11,15 @@ public class LeetCodeSortestElement {
 
         LeetCodeSortestElement leetCodeSortestElement = new LeetCodeSortestElement();
 
-       // int[][] arr = {{1, 5, 9}, {10, 11, 13}, {12, 13, 15}};
+        // int[][] arr = {{1, 5, 9}, {10, 11, 13}, {12, 13, 15}};
 
-       // System.out.println(leetCodeSortestElement.kthSmallest(arr, 8));
+        // System.out.println(leetCodeSortestElement.kthSmallest(arr, 8));
 
 
-        int[] a = {1, 3};
+        int[] a = {1, 1,2};
         int[] b = {2};
 
-        System.out.println(leetCodeSortestElement.findMedianSortedArrays(a, b));
+        System.out.println(leetCodeSortestElement.removeDuplicates(a));
     }
 
 
@@ -59,17 +56,122 @@ public class LeetCodeSortestElement {
         Collections.sort(list);
 
         if (list.size() % 2 != 0) {
-            double d = Double.valueOf(list.get((list.size()/2)+1));
-           // double ciel =  Math.ceil(d);
+            double d = Double.valueOf(list.get((list.size() / 2) + 1));
+            // double ciel =  Math.ceil(d);
 
             return d;
 
-        }else if(list.size() % 2 == 0){
-            int center = list.size() /2;
-            double d = Double.valueOf(list.get(center)+list.get(center-1)) / 2;
+        } else if (list.size() % 2 == 0) {
+            int center = list.size() / 2;
+            double d = Double.valueOf(list.get(center) + list.get(center - 1)) / 2;
             return d;
         }
 
         return 0.0;
     }
+
+
+    public List< List< Integer > > threeSum ( int[] nums ) {
+
+        List< List<Integer> > list = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i + 1 < nums.length && i + 2 < nums.length) {
+
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                for (int k = 0; k < nums.length; k++) {
+
+                    if ((i != j && i != k && j != k) && (nums[i] + nums[j] + nums[k] == 0)){
+                        List<Integer> li = new ArrayList<>();
+                        li.add(nums[i] );
+                        li.add(nums[j]);
+                        li.add(nums[k]);
+
+                        list.add(li);
+                    }
+
+                }
+            }
+        }
+
+        return list;
+
+    }
+
+
+    public int removeDuplicates(int[] nums) {
+
+        List<Integer> l = new ArrayList<>();
+
+        for(int i : nums) {
+            if (!l.contains(i)) {
+                l.add(i);
+            }
+        }
+            int size = l.size();
+            int[] nums1 = new int[size];
+
+            for(int i =0 ; i < size; i++){
+                nums1[i] = l.get(i);
+            }
+
+            for(int i : nums1){
+                System.out.print(i+" ,");
+            }
+
+            nums = nums1;
+
+            return size;
+    }
+
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        for(String s : strs){
+
+            char[] a = s.toCharArray();
+            Arrays.sort(a);
+
+            String s1 = String.valueOf(a);
+
+            if(map.containsKey(s1)){
+                List<String> sList = map.get(s1);
+                sList.add(s);
+                map.put(s1, sList);
+            }else{
+
+                List<String> list = new ArrayList<>();
+                list.add(s);
+                map.put(s1, list);
+            }
+        }
+
+        List<List<String>> mainList = new ArrayList<>();
+
+        for(Map.Entry<String, List<String>> m1 : map.entrySet()){
+            mainList.add(m1.getValue());
+        }
+
+        return mainList;
+
+    }
+
+
+    public void rotate(int[][] matrix) {
+
+        int[][] arr = matrix;
+
+
+        for()
+
+
+
+    }
 }
+
