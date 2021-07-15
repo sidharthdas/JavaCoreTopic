e8a1dada913cc1a656af6f933a3b3e05c4368a4d
Sidharth <sidharthdas.1995@gmail.com>
2021-07-15 22:34:51 +0530

    leetcode

--- JavaCoreTopics/src/JUNE/practice1/LeetCode14thJuly.java
+++ JavaCoreTopics/src/JUNE/practice1/LeetCode14thJuly.java
@@ -1,5 +1,6 @@
 package JUNE.practice1;
 
+import java.math.BigInteger;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -17,8 +18,15 @@ public class LeetCode14thJuly {
 
     public static void main ( String[] args ) {
         LeetCode14thJuly leetCode14thJuly = new LeetCode14thJuly();
+        int[] arr = {3, 2, 4};
 
-        System.out.println(leetCode14thJuly.frequencySort("tree"));
+        String[] words = {"ad", "bd", "aaab", "baa", "badab"};
+        String allowed = "ab";
+
+        int[] arr1 = {2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19};
+        int[] arr2 = {2, 1, 4, 3, 9, 6};
+
+        System.out.println(leetCode14thJuly.relativeSortArray(arr1, arr2));
 
     }
 
@@ -295,23 +303,23 @@ public class LeetCode14thJuly {
                 .sorted((Map.Entry.< String, Integer >comparingByValue().reversed()))
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, ( e1, e2 ) -> e1, LinkedHashMap::new));
 
-        for(Map.Entry<String, Integer> m : sortedByCount.entrySet()){
+        for (Map.Entry< String, Integer > m : sortedByCount.entrySet()) {
 
-            if(!list.contains(m.getKey())){
+            if (!list.contains(m.getKey())) {
                 return m.getKey();
             }
         }
         return null;
     }
 
-    public String frequencySort1(String s) {
+    public String frequencySort1 ( String s ) {
         String[] str = s.split("");
 
         Map< String, Integer > map = new HashMap<>();
-        for (String ss: str) {
-            if(map.containsKey(ss)){
-                map.put(ss, map.get(ss)+1);
-            }else{
+        for (String ss : str) {
+            if (map.containsKey(ss)) {
+                map.put(ss, map.get(ss) + 1);
+            } else {
                 map.put(ss, 1);
             }
         }
@@ -323,8 +331,8 @@ public class LeetCode14thJuly {
 
         String res = "";
 
-        for(Map.Entry<String, Integer> m : sortedByCount.entrySet()){
-            for(int i = 0; i < m.getValue(); i ++ ){
+        for (Map.Entry< String, Integer > m : sortedByCount.entrySet()) {
+            for (int i = 0; i < m.getValue(); i++) {
                 res += m.getKey();
             }
         }
@@ -332,7 +340,227 @@ public class LeetCode14thJuly {
         return res;
     }
 
-    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+    public int busyStudent ( int[] startTime, int[] endTime, int queryTime ) {
 
+        int count = 0;
+
+        int length = startTime.length;
+
+        for (int i = 0; i < length; i++) {
+            if (queryTime >= startTime[i] && queryTime <= endTime[i]) {
+                count += 1;
+            }
+        }
+
+        return count;
+    }
+
+
+    public int[] twoSum ( int[] nums, int target ) {
+        int[] ind = new int[2];
+        int index = 0;
+        int len = nums.length;
+        int firstValue = nums[0];
+        for (int i = 0; i <= len; i++) {
+            if (i < len && i != index) {
+                if (firstValue + nums[i] == target) {
+                    ind[0] = index;
+                    ind[1] = i;
+                    return ind;
+                } else if (i == len - 1) {
+                    index += 1;
+                    firstValue = nums[index];
+                    i = 0;
+                    System.out.println(i);
+
+                }
+            }
+
+        }
+        return null;
+    }
+
+
+    public String[] findRestaurant ( String[] list1, String[] list2 ) {
+
+        List< String > l1 = new ArrayList<>();
+        for (String s : list1) {
+            l1.add(s);
+        }
+
+
+        List< String > l2 = new ArrayList<>();
+        for (String s : list2) {
+            l2.add(s);
+        }
+
+        List< String > res = new ArrayList<>();
+
+        for (String s : l1) {
+            if (l2.contains(s)) {
+                res.add(s);
+            }
+        }
+
+        int size = res.size();
+        String[] result = new String[size];
+
+        for (int i = 0; i < size; i++) {
+            result[i] = res.get(i);
+        }
+
+        return result;
+    }
+
+
+    public String[] uncommonFromSentences ( String s1, String s2 ) {
+
+        Map< String, Integer > map1 = new HashMap<>();
+        Map< String, Integer > map2 = new HashMap<>();
+
+        String[] s1tr = s1.split(" ");
+        String[] s2tr = s2.split(" ");
+
+
+        for (String s : s1tr) {
+            if (map1.containsKey(s)) {
+
+                map1.put(s, map1.get(s) + 1);
+            } else {
+                map1.put(s, 1);
+            }
+
+        }
+
+        for (String s : s2tr) {
+            if (map2.containsKey(s)) {
+
+                map2.put(s, map2.get(s) + 1);
+            } else {
+                map2.put(s, 1);
+            }
+
+        }
+        List< String > res = new ArrayList<>();
+
+        for (Map.Entry< String, Integer > m : map1.entrySet()) {
+            if (m.getValue() == 1) {
+                if (!map2.containsKey(m.getKey())) {
+                    res.add(m.getKey());
+                }
+            }
+        }
+
+        for (Map.Entry< String, Integer > m : map2.entrySet()) {
+            if (m.getValue() == 1) {
+                if (!map1.containsKey(m.getKey())) {
+                    res.add(m.getKey());
+                }
+            }
+        }
+
+
+        int size = res.size();
+
+        String[] result = new String[size];
+
+        for (int i = 0; i < size; i++) {
+            result[i] = res.get(i);
+        }
+
+        return result;
+    }
+
+    public String largestOddNumber ( String num ) {
+
+        while (num.length() != 0) {
+
+            if (Integer.parseInt(String.valueOf(num.charAt(num.length() - 1))) % 2 != 0) {
+                return num;
+            } else {
+                num = num.substring(0, num.length() - 1);
+            }
+        }
+        return num;
+    }
+
+    public int countConsistentStrings ( String allowed, String[] words ) {
+        List< String > list = new ArrayList<>();
+        for (String s : words) {
+            String str[] = allowed.split("");
+            for (String s1 : str) {
+                s = s.replaceAll(s1, "");
+            }
+            list.add(s);
+        }
+
+        int count = 0;
+
+        for (String s : list) {
+            if (s.equals("")) {
+                count += 1;
+            }
+        }
+
+
+        return count;
+
+    }
+
+    public int[] relativeSortArray ( int[] arr1, int[] arr2 ) {
+
+        Map< Integer, Integer > map = new HashMap<>();
+
+        for (int i : arr1) {
+            if (map.containsKey(i)) {
+                map.put(i, map.get(i) + 1);
+            } else {
+                map.put(i, 1);
+            }
+        }
+        List< Integer > l = new ArrayList<>();
+        List< Integer > not = new ArrayList<>();
+        for (int i : arr2) {
+            if (map.containsKey(i)) {
+                int j = map.get(i);
+                for (int k = 0; k < j; k++) {
+                    l.add(i);
+                }
+            }
+        }
+        List< Integer > arr2List = new ArrayList<>();
+        for (int i : arr2) {
+            arr2List.add(i);
+        }
+        System.out.println(map.keySet());
+        System.out.println(map.keySet().contains(7));
+        for (int i : arr1) {
+            System.out.println(i);
+            if (!arr2List.contains(i)) {
+                not.add(i);
+            }
+        }
+        Collections.sort(not);
+
+        l.addAll(not);
+        int finalSize = l.size();
+        int[] res = new int[finalSize];
+
+        for (int i = 0; i < finalSize; i++) {
+            res[i] = l.get(i);
+        }
+        return res;
+    }
+
+    public int findNumbers ( int[] nums ) {
+
+        int count = 0;
+
+        for (int i : nums) {
+            if (String.valueOf(i).length() % 2 == 0) {
+                count += 1;
+            }
+        }
+        return count;
     }
 }
