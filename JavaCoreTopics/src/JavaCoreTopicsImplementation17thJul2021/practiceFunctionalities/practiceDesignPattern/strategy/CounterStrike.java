e664ff9a60e2efbcdc8947dce46c1c20bdec7726
Sidharth <sidharthdas.1995@gmail.com>
2021-07-22 23:11:51 +0530

    Practice

--- JavaCoreTopics/src/JavaCoreTopicsImplementation17thJul2021/practiceFunctionalities/practiceDesignPattern/strategy/CounterStrike.java
+++ JavaCoreTopics/src/JavaCoreTopicsImplementation17thJul2021/practiceFunctionalities/practiceDesignPattern/strategy/CounterStrike.java
@@ -8,7 +8,24 @@ public class CounterStrike {
 
     public static void main ( String[] args ) {
 
+        System.out.println("Before Bomb");
 
+        Player terrorist = new Player("Terrorist");
+        terrorist.strategy = new DefenceStrategy();
+
+        Player cs = new Player("Counter Terrorist");
+        cs.strategy = new AgressiveStrategy();
+
+        terrorist.action();
+        cs.action();
+
+        System.out.println("After Bomb");
+
+        terrorist.strategy = new AgressiveStrategy();
+        cs.strategy = new DefenceStrategy();
+
+        terrorist.action();
+        cs.action();
 
     }
 }
