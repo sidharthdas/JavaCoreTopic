05ef76f776797a9d925d0027ece120cc3a2f5a7f
Sidharth <sidharthdas.1995@gmail.com>
2021-07-28 22:34:10 +0530

    tech

--- JavaCoreTopics/src/JavaCoreTopicsImplementation17thJul2021/abstractclass/AbstractClassMainClass.java
+++ JavaCoreTopics/src/JavaCoreTopicsImplementation17thJul2021/abstractclass/AbstractClassMainClass.java
@@ -4,7 +4,7 @@ package JavaCoreTopicsImplementation17thJul2021.abstractclass;
  * @author sidharthdas on 18/07/21.
  */
 
-abstract class A{
+abstract class A implements I{
 
     public A(){
 
---
+++ JavaCoreTopics/src/JavaCoreTopicsImplementation17thJul2021/abstractclass/I.java
@@ -0,0 +1,7 @@
+package JavaCoreTopicsImplementation17thJul2021.abstractclass;
+
+/**
+ * @author sidharthdas on 27/07/21.
+ */
+public interface I{
+}
