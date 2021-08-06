f909c7a3a06edfd266119925c6d147c603815598
Sidharth <sidharthdas.1995@gmail.com>
2021-08-06 22:12:44 +0530

    practice

---
+++ JavaCoreTopics/src/JavaCoreTopicsImplementation17thJul2021/practice6thAugust2021/designpartern/bilder/MainClass.java
@@ -0,0 +1,12 @@
+package JavaCoreTopicsImplementation17thJul2021.practice6thAugust2021.designpartern.bilder;
+
+/**
+ * @author sidharthdas on 06/08/21.
+ */
+public class MainClass {
+
+    public static void main ( String[] args ) {
+
+        Laptop laptop = new Laptop.LaptopBuilder(1,"256", "512").build();
+    }
+}
