a1ae4ff6d30965e4c68905fe59e18530d71c6d1d
Sidharth <sidharthdas.1995@gmail.com>
2021-09-13 22:23:04 +0530

    leetcode

--- JavaCoreTopics/src/JavaCoreTopicsImplementation17thJul2021/practice/practice08sept2021/leetcode/MainClass.java
+++ JavaCoreTopics/src/JavaCoreTopicsImplementation17thJul2021/practice/practice08sept2021/leetcode/MainClass.java
@@ -9,7 +9,7 @@ public class MainClass {
 
 
     public static void main ( String[] args ) {
-        //System.out.println((Double.valueOf(12)/Double.valueOf(25));
+       // System.out.println((Double.valueOf(12)/Double.valueOf(25));
 
     }
 
