5845e1ab06b9a32bacbb140a05fcca3b2437608d
sidharthdas <sidharthdas.1995@gmail.com>
2022-10-24 22:35:57 +0530

    Added LEETCODE SOLUTIONS

---
+++ JavaCoreTopics/src/Oct4Th2022/leetcode/LeeetcodeMain15.java
@@ -0,0 +1,49 @@
+package Oct4Th2022.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author Sidharth Das
+ * created on  24/10/22
+ */
+public class LeeetcodeMain15 {
+
+    public static void main(String[] args) {
+
+        // Note that the values ordered by ID is ["aaaaa", "bbbbb", "ccccc", "ddddd", "eeeee"].
+        OrderedStream os = new OrderedStream(5);
+        os.insert(3, "ccccc"); // Inserts (3, "ccccc"), returns [].
+        os.insert(1, "aaaaa"); // Inserts (1, "aaaaa"), returns ["aaaaa"].
+        os.insert(2, "bbbbb"); // Inserts (2, "bbbbb"), returns ["bbbbb", "ccccc"].
+        os.insert(5, "eeeee"); // Inserts (5, "eeeee"), returns [].
+        os.insert(4, "ddddd"); // Inserts (4, "ddddd"), returns ["ddddd", "eeeee"].
+// Concatentating all the chunks returned:
+// [] + ["aaaaa"] + ["bbbbb", "ccccc"] + [] + ["ddddd", "eeeee"] = ["aaaaa", "bbbbb", "ccccc", "ddddd", "eeeee"]
+// The resulting order is the same as the order above.
+
+    }
+}
+
+class OrderedStream {
+
+    String[] list;
+    int pointer;
+
+    public OrderedStream(int n) {
+        list = new String[n];
+        this.pointer = 0;
+    }
+
+    public List<String> insert(int idKey, String value) {
+        list[idKey - 1] = value;
+        List<String> newList = new ArrayList<>();
+
+        while(pointer < list.length && Objects.nonNull(list[pointer])){
+            newList.add(list[pointer]);
+            pointer += 1;
+        }
+        return newList;
+    }
+}
