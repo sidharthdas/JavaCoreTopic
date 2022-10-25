cc791c6c147ea13c46e4816b1f687e3253fa3353
sidharthdas <sidharthdas.1995@gmail.com>
2022-10-25 14:07:47 +0530

    Added LEETCODE SOLUTIONS

--- JavaCoreTopics/src/Oct4Th2022/leetcode/LeeetcodeMain15.java
+++ JavaCoreTopics/src/Oct4Th2022/leetcode/LeeetcodeMain15.java
@@ -1,8 +1,6 @@
 package Oct4Th2022.leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 /**
  * @author Sidharth Das
@@ -23,6 +21,82 @@ public class LeeetcodeMain15 {
 // [] + ["aaaaa"] + ["bbbbb", "ccccc"] + [] + ["ddddd", "eeeee"] = ["aaaaa", "bbbbb", "ccccc", "ddddd", "eeeee"]
 // The resulting order is the same as the order above.
 
+    }
+
+    public int numUniqueEmails(String[] emails) {
+        Map<String , Integer> map = new HashMap<>();
+
+        for(String email : emails){
+            String subLocal = email.substring(0, email.indexOf("@") + 1);
+            String em = subLocal.replace(".", "");
+            if(em.contains("+")){
+                String sub = em.substring(em.indexOf("+"), em.indexOf("@"));
+                em = em.replace(sub, "");
+            }
+
+            String mainEmail = em + email.substring(email.indexOf("@")+1);
+            System.out.println(mainEmail);
+            map.put(em, map.getOrDefault(mainEmail, 0) + 1);
+        }
+
+        return (int) map.entrySet().stream().count();
+
+    }
+
+    public int findMaxK(int[] nums) {
+        List<Integer> l1 = Arrays.stream(nums).boxed().toList();
+        List<Integer> l = new ArrayList<>();
+        for(int x : l1){
+            if(x > 0){
+                l.add(x);
+            }
+        }
+        Collections.sort(l, Collections.reverseOrder());
+
+        for(int x : l){
+            if(l1.contains(-1 * x)){
+                return x;
+            }
+        }
+        return -1;
+    }
+
+    public String destCity(List<List<String>> paths) {
+        Set<String> set = new HashSet<>();
+        for(List<String> path :  paths){
+            set.add(path.get(1));
+        }
+        for(List<String> path :  paths){
+            set.remove(path.get(0));
+        }
+
+        return set.stream().findFirst().get();
+
+    }
+    public int[] numberOfLines(int[] widths, String s) {
+
+        int totalWidths = Arrays.stream(widths).boxed().reduce(0, Integer::sum);
+        int count = 0;
+
+        return null;
+    }
+
+    public int maxDistance(int[] colors) {
+        int maxD = Integer.MIN_VALUE;
+        int len = colors.length;
+
+        for(int i = 0; i < len; i++){
+
+            for(int j = len - 1; j >= 0; j--){
+                if(colors[j] != colors[i]){
+                    if((j - i) > maxD){
+                        maxD = j-i;
+                        break;
+                    }
+                }
+            }
+        }
+        return maxD;
     }
 
     public int sumOddLengthSubarrays(int[] arr) {
