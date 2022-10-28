de7fe39cd19243f6c4c34b8e1eff58a812470a09
sidharthdas <sidharthdas.1995@gmail.com>
2022-10-28 12:27:14 +0530

    Added LEETCODE SOLUTIONS

--- JavaCoreTopics/src/Oct4Th2022/leetcode/LeetcodeMainClass16.java
+++ JavaCoreTopics/src/Oct4Th2022/leetcode/LeetcodeMainClass16.java
@@ -40,9 +40,26 @@ public class LeetcodeMainClass16 {
         streamChecker.query("j"); // return False
         streamChecker.query("k"); // return False
         streamChecker.query("l"); // return True, because 'kl' is in the wordlist*/
+        countQuadruplets_1(new int[]{1,2,3,6});
     }
 
-    //
+    public static int countQuadruplets_1(int[] nums) {
+        int len = nums.length;
+        int res = 0;
+        for(int i = 0; i < len; i++){
+            for(int j = i+ 1; j < len; j++){
+                for(int k = i + 2; k < len; k++){
+                    for(int l = i + 3; l < len; l++){
+                        if(nums[i] + nums[j] + nums[k] == nums[l]){
+                            res += 1;
+                        }
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
     public int nextBeautifulNumber(int n) {
         int[] beautifulNumbers = {0, 1, 22, 122, 212, 221, 333, 1333, 3133, 3313, 3331, 4444, 14444, 22333, 23233, 23323, 23332, 32233, 32323, 32332, 33223, 33232, 33322, 41444, 44144, 44414, 44441, 55555, 122333, 123233, 123323, 123332, 132233, 132323, 132332, 133223, 133232, 133322, 155555, 212333, 213233, 213323, 213332, 221333, 223133, 223313, 223331, 224444, 231233, 231323, 231332, 232133, 232313, 232331, 233123, 233132, 233213, 233231, 233312, 233321, 242444, 244244, 244424, 244442, 312233, 312323, 312332, 313223, 313232, 313322, 321233, 321323, 321332, 322133, 322313, 322331, 323123, 323132, 323213, 323231, 323312, 323321, 331223, 331232, 331322, 332123, 332132, 332213, 332231, 332312, 332321, 333122, 333212, 333221, 422444, 424244, 424424, 424442, 442244, 442424, 442442, 444224, 444242, 444422, 515555, 551555, 555155, 555515, 555551, 666666, 1224444};
         int position = Arrays.binarySearch(beautifulNumbers, n);
