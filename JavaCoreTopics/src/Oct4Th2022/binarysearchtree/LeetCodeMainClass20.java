72e13e5653cf068372f6d6ce6239c1283dc7a404
sidharthdas <sidharthdas.1995@gmail.com>
2022-11-07 10:56:00 +0530

    Added leetcode Solution

--- JavaCoreTopics/src/Oct4Th2022/binarysearchtree/LeetCodeMainClass20.java
+++ JavaCoreTopics/src/Oct4Th2022/binarysearchtree/LeetCodeMainClass20.java
@@ -16,6 +16,20 @@ public class LeetCodeMainClass20 {
         System.out.println(minimumCost(new int[]{1, 2, 3}));
     }
 
+    public int maximum69Number (int num) {
+        char[] arr = String.valueOf(num).toCharArray();
+        int len = arr.length;
+
+        for(int i = 0; i < len ; i++){
+            if(arr[i] == '6'){
+                arr[i] = '9';
+                break;
+            }
+        }
+
+        return Integer.parseInt(String.valueOf(arr));
+    }
+
     public List<Integer> intersection(int[][] nums) {
 
         Map<Integer, Integer> map = new TreeMap<>();
