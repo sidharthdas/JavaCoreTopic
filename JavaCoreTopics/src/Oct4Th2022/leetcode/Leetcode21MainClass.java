a5db406905e6128a314a78bde78f3e0d4b058cb8
sidharthdas <sidharthdas.1995@gmail.com>
2022-11-24 21:15:14 +0530

    Added leetcode Solution

--- JavaCoreTopics/src/Oct4Th2022/leetcode/Leetcode21MainClass.java
+++ JavaCoreTopics/src/Oct4Th2022/leetcode/Leetcode21MainClass.java
@@ -48,6 +48,25 @@ public class Leetcode21MainClass {
 
     }
 
+    public int distinctAverages(int[] nums) {
+
+        List<Integer> list = Arrays.stream(nums).boxed().collect(Collectors.toList());
+        Set<Double> set = new TreeSet<>();
+
+        while (list.size() != 0) {
+            list = list.stream().sorted().collect(Collectors.toList());
+            int max = list.get(list.size() - 1);
+            int min = list.get(0);
+            list.remove(new Integer(max));
+            list.remove(new Integer(min));
+
+            double d = (max + min) / 2;
+            set.add(d);
+        }
+
+        return set.size();
+    }
+
     public static int[] applyOperations(int[] nums) {
         int len = nums.length;
 
