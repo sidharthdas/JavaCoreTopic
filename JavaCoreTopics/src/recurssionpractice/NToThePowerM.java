7bd3f7752080cb3c19e107d8c14dfe41708cb9f7
sidharthdas <sidharthdas.1995@gmail.com>
2022-12-07 00:37:41 +0530

    Added leetcode Solution

--- JavaCoreTopics/src/recurssionpractice/NToThePowerM.java
+++ JavaCoreTopics/src/recurssionpractice/NToThePowerM.java
@@ -7,10 +7,14 @@ package recurssionpractice;
 public class NToThePowerM {
 
     public static void main(String[] args) {
-
+        System.out.println(power(2, 3));
     }
 
     public static int power(int n, int m){
-        return 0;
+        if(m == 0){
+            return 1;
+        }
+
+        return n * power(n, m - 1);
     }
 }
