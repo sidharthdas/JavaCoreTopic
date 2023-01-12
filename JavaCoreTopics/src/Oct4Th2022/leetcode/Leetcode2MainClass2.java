b119c86e3781bc4e659bfc8ba043a23c8e92cdb9
sidharthdas <sidharthdas.1995@gmail.com>
2023-01-12 21:35:15 +0530

    Added leetcode Solution

--- JavaCoreTopics/src/Oct4Th2022/leetcode/Leetcode2MainClass2.java
+++ JavaCoreTopics/src/Oct4Th2022/leetcode/Leetcode2MainClass2.java
@@ -1,9 +1,6 @@
 package Oct4Th2022.leetcode;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author Sidharth Das
@@ -18,7 +15,7 @@ public class Leetcode2MainClass2 {
     public int[] findingUsersActiveMinutes(int[][] logs, int k) {
         Map<Integer, Set<Integer>> map = new HashMap<>();
         int[] finalRes = new int[k];
-        for(int[] log : logs){
+        for (int[] log : logs) {
             map.putIfAbsent(log[0], new HashSet<>());
             map.get(log[0]).add(log[1]);
         }
@@ -27,5 +24,31 @@ public class Leetcode2MainClass2 {
             finalRes[map.get(key).size() - 1]++;
         }
         return finalRes;
+    }
+
+    public long dividePlayers(int[] skill) {
+        Arrays.sort(skill);
+        int len = skill.length;
+        int first = 0;
+        int last = len - 1;
+        List<List<Integer>> l =new ArrayList<>();
+        int same = skill[0] + skill[len - 1];
+        while(first < last) {
+            if((skill[first] + skill[last] )!= same){
+                return -1;
+            }
+            l.add(List.of(skill[first], skill[last]));
+            first++;
+            last--;
+
+        }
+
+        int finalVal = 0;
+
+        for(List<Integer> l1 : l){
+            finalVal += (l1.get(0) * l1.get(1));
+        }
+
+        return finalVal;
     }
 }
