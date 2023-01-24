207576c17994f93c8641898da9e11d0ddffe832a
sidharthdas <sidharthdas.1995@gmail.com>
2023-01-24 13:04:22 +0530

    Added leetcode Solution

--- JavaCoreTopics/src/Oct4Th2022/leetcode/Leetcode24MainClass.java
+++ JavaCoreTopics/src/Oct4Th2022/leetcode/Leetcode24MainClass.java
@@ -1,6 +1,7 @@
 package Oct4Th2022.leetcode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -11,6 +12,33 @@ public class Leetcode24MainClass {
 
     public static void main(String[] args) {
 
+    }
+
+    public int differenceOfSum(int[] nums) {
+
+        int totalSum = Arrays.stream(nums).boxed().reduce(Integer::sum).get();
+        int digitSum = 0;
+        for (int i : nums) {
+            if (i < 10) {
+                digitSum += i;
+            } else {
+                digitSum += getSum(i);
+            }
+        }
+
+        return  Math.abs(totalSum - digitSum);
+    }
+
+    private  int getSum(int n)
+    {
+        int sum = 0;
+
+        while (n != 0) {
+            sum = sum + n % 10;
+            n = n / 10;
+        }
+
+        return sum;
     }
 }
 
@@ -27,23 +55,23 @@ class DataStream {
 
     public boolean consec(int num) {
         l.add(num);
-        if(l.size() < k){
+        if (l.size() < k) {
             return false;
         }
         int lastIn = l.size() - 1;
         int count = 0;
-        while(lastIn >= 0){
-            if(l.get(lastIn) == value){
-                count ++;
-                if(count == k){
+        while (lastIn >= 0) {
+            if (l.get(lastIn) == value) {
+                count++;
+                if (count == k) {
                     return true;
                 }
-            }else{
-                if(count != k){
+            } else {
+                if (count != k) {
                     break;
                 }
             }
-            lastIn --;
+            lastIn--;
         }
 
         return false;
