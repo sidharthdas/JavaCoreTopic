407e5029d0508f11b46043d56a71c8001afd7918
sidharthdas <sidharthdas.1995@gmail.com>
2023-02-10 12:10:25 +0530

    Added leetcode Solution

--- JavaCoreTopics/src/Oct4Th2022/leetcode/Leetcode25MainClass.java
+++ JavaCoreTopics/src/Oct4Th2022/leetcode/Leetcode25MainClass.java
@@ -8,6 +8,24 @@ import java.util.List;
  * created on  10/02/23
  */
 public class Leetcode25MainClass {
+
+    public static void main(String[] args) {
+        Cashier cashier = new Cashier(3, 50, new int[]{1, 2, 3, 4, 5, 6, 7}, new int[]{100, 200, 300, 400, 300, 200, 100});
+
+        cashier.getBill(new int[]{1, 2}, new int[]{1, 2});                        // return 500.0. 1st customer, no discount.
+        // bill = 1 * 100 + 2 * 200 = 500.
+        cashier.getBill(new int[]{3, 7}, new int[]{10, 10});                      // return 4000.0. 2nd customer, no discount.
+        // bill = 10 * 300 + 10 * 100 = 4000.
+        cashier.getBill(new int[]{1, 2, 3, 4, 5, 6, 7}, new int[]{1, 1, 1, 1, 1, 1, 1});    // return 800.0. 3rd customer, 50% discount.
+        // Original bill = 1600
+        // Actual bill = 1600 * ((100 - 50) / 100) = 800.
+        cashier.getBill(new int[]{4}, new int[]{10});                           // return 4000.0. 4th customer, no discount.
+        cashier.getBill(new int[]{7, 3}, new int[]{10, 10});                      // return 4000.0. 5th customer, no discount.
+        cashier.getBill(new int[]{7, 5, 3, 1, 6, 4, 2}, new int[]{10, 10, 10, 9, 9, 9, 7}); // return 7350.0. 6th customer, 50% discount.
+        // Original bill = 14700, but with
+        // Actual bill = 14700 * ((100 - 50) / 100) = 7350.
+        cashier.getBill(new int[]{2, 3, 5}, new int[]{5, 3, 2});                    // return 2500.0.  6th customer, no discount.
+    }
 }
 
 class Cashier {
@@ -34,7 +52,7 @@ class Cashier {
         }
 
         if (i == 3) {
-            sum = sum * ((100 - discount) / 100);
+            sum = sum * ((100d - Double.valueOf(discount)) / 100d);
             i = 0;
         }
 
