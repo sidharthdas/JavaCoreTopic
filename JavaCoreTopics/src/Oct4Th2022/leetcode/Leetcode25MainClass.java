751887a38d7de1e1579bd9f7783d995d5de4c0a9
sidharthdas <sidharthdas.1995@gmail.com>
2023-02-14 18:42:50 +0530

    Added leetcode Solution

--- JavaCoreTopics/src/Oct4Th2022/leetcode/Leetcode25MainClass.java
+++ JavaCoreTopics/src/Oct4Th2022/leetcode/Leetcode25MainClass.java
@@ -32,6 +32,34 @@ public class Leetcode25MainClass {
         System.out.println(largeGroupPositions("abcdddeeeeaabbbcd"));
     }
 
+    public boolean lemonadeChange(int[] bills) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(5, 0);
+        map.put(10, 0);
+        for (int bill : bills) {
+            if (bill == 5) {
+                map.put(5, map.get(5) + 1);
+            } else if (bill == 10 && map.get(5) >= 1) {
+                map.put(5, map.get(5) - 1);
+                map.put(10, map.get(10) + 1);
+            } else if (bill == 20) {
+                if (map.get(5) != 0 && map.get(10) != 0) {
+                    map.put(5, map.get(5) - 1);
+                    map.put(10, map.get(10) - 1);
+                } else if( map.get(5) >= 3){
+                    map.put(5, map.get(5) - 3);
+                }else{
+                    return false;
+                }
+
+            } else{
+                return false;
+            }
+        }
+        return true;
+
+    }
+
     public static List<List<Integer>> largeGroupPositions(String s) {
 
         String[] srr = s.split("");
@@ -42,10 +70,10 @@ public class Leetcode25MainClass {
         int lIndex = 0;
         List<List<Integer>> l = new ArrayList<>();
         for (int i = 1; i < len; i++) {
-            if(currString.equals(srr[i])){
-                count ++;
+            if (currString.equals(srr[i])) {
+                count++;
             } else {
-                if(count >= 3){
+                if (count >= 3) {
                     l.add(List.of(sIndex, i - 1));
                 }
                 count = 1;
@@ -54,7 +82,7 @@ public class Leetcode25MainClass {
             }
         }
 
-        if(count >= 3){
+        if (count >= 3) {
             l.add(List.of(sIndex, len - 1));
         }
 
