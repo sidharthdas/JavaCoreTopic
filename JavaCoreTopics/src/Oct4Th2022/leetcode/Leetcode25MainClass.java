7ee2d59c1732f8d8c29a238bc69e0d6edca9e1bb
sidharthdas <sidharthdas.1995@gmail.com>
2023-02-16 12:17:18 +0530

    Added leetcode Solution

--- JavaCoreTopics/src/Oct4Th2022/leetcode/Leetcode25MainClass.java
+++ JavaCoreTopics/src/Oct4Th2022/leetcode/Leetcode25MainClass.java
@@ -45,7 +45,7 @@ public class Leetcode25MainClass {
 
     public static int[] getNoZeroIntegers(int n) {
         for (int i = 1; i <=n; i++){
-            if(!String.valueOf(n - i).contains("0")){
+            if(!String.valueOf(i).contains("0") &&!String.valueOf(n - i).contains("0")){
                 return new int[]{i, n-i};
             }
         }
