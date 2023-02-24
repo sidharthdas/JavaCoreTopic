48b9e2cc24710c340eb682eea0b8b38557197af9
sidharthdas <sidharthdas.1995@gmail.com>
2023-02-24 12:51:30 +0530

    Added leetcode Solution

--- JavaCoreTopics/src/Oct4Th2022/leetcode/LeetcodeMainClass26.java
+++ JavaCoreTopics/src/Oct4Th2022/leetcode/LeetcodeMainClass26.java
@@ -15,7 +15,7 @@ public class LeetcodeMainClass26 {
 [1,1,0,0]
 sandwiches =
 [0,1,0,1]*/
-        System.out.println(countStudents(new int[]{1,1,0,0}, new int[]{0,1,0,1}));
+        System.out.println(countStudents(new int[]{1,1,1,0,0,1}, new int[]{1,0,0,0,1,1}));
     }
 
     public static int countStudents(int[] students, int[] sandwiches) {
@@ -29,12 +29,19 @@ sandwiches =
         }
         while ((!((count(students, 1) != 0 && count(sandwiches, 1) == 0) ||
                 (count(students, 0) != 0 && count(sandwiches, 0) == 0)))
-                && !sandwichesQueue.isEmpty()) {
+                || !sandwichesQueue.isEmpty()) {
             if (studentsQueue.peek() == sandwichesQueue.peek()) {
                 studentsQueue.poll();
                 sandwichesQueue.poll();
             } else {
                 studentsQueue.add(studentsQueue.poll());
+            }
+
+            if(sandwichesQueue.isEmpty()){
+                break;
+            }
+            if(count(students, sandwichesQueue.peek()) == 0){
+                break;
             }
         }
         return studentsQueue.size();
