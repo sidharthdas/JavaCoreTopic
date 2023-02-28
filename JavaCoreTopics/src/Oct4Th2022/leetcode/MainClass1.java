38665f670c92820d547bbf35d7e92fb782612e04
sidharthdas <sidharthdas.1995@gmail.com>
2023-02-28 09:57:53 +0530

    Added leetcode Solution

---
+++ JavaCoreTopics/src/Oct4Th2022/leetcode/MainClass1.java
@@ -0,0 +1,12 @@
+package Oct4Th2022.leetcode;
+
+/**
+ * @author Sidharth Das
+ * created on  27/02/23
+ */
+public class MainClass1 {
+
+    public static void main(String[] args) {
+        //
+    }
+}
