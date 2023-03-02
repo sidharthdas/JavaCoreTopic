c10530082d4db2f77491b941962d13b24ede40ae
sidharthdas <sidharthdas.1995@gmail.com>
2023-03-02 13:57:14 +0530

    Leetcode

--- JavaCoreTopics/src/Oct4Th2022/leetcode/MainClass1.java
+++ JavaCoreTopics/src/Oct4Th2022/leetcode/MainClass1.java
@@ -7,21 +7,6 @@ package Oct4Th2022.leetcode;
 public class MainClass1 {
 
     public static void main(String[] args) {
-        //Kadane Algo
 
-        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-
-        int sum = 0;
-        int maxi = arr[0];
-        int len = arr.length;
-        for (int i = 0; i < len; i++) {
-            sum += arr[i];
-            maxi = Math.max(maxi, sum);
-
-            if(sum < 0){
-                sum = 0;
-            }
-        }
-        System.out.println(maxi);
     }
 }
