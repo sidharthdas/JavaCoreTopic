c41c88b8b27768c3d42097391bcacec5f2689184
sidharthdas <sidharthdas.1995@gmail.com>
2023-03-04 00:28:41 +0530

    Leetcode

--- JavaCoreTopics/src/Oct4Th2022/leetcode/Leetcode27Mainclass.java
+++ JavaCoreTopics/src/Oct4Th2022/leetcode/Leetcode27Mainclass.java
@@ -3,6 +3,7 @@ package Oct4Th2022.leetcode;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
+import java.util.stream.Collectors;
 
 /**
  * @author Sidharth Das
@@ -11,6 +12,20 @@ import java.util.Stack;
 public class Leetcode27Mainclass {
 
     public static void main(String[] args) {
+
+        CustomStack1 stk = new CustomStack1(3); // Stack is Empty []
+        stk.push(1);                          // stack becomes [1]
+        stk.push(2);                          // stack becomes [1, 2]
+        stk.pop();                            // return 2 --> Return top of the stack 2, stack becomes [1]
+        stk.push(2);                          // stack becomes [1, 2]
+        stk.push(3);                          // stack becomes [1, 2, 3]
+        stk.push(4);                          // stack still [1, 2, 3], Do not add another elements as size is 4
+        stk.increment(5, 100);                // stack becomes [101, 102, 103]
+        stk.increment(2, 100);                // stack becomes [201, 202, 103]
+        stk.pop();                            // return 103 --> Return top of the stack 103, stack becomes [201, 202]
+        stk.pop();                            // return 202 --> Return top of the stack 202, stack becomes [201]
+        stk.pop();                            // return 201 --> Return top of the stack 201, stack becomes []
+        stk.pop();                            // return -1 --> Stack is empty return -1.
 
     }
 
@@ -138,23 +153,52 @@ class BST1 {
 }
 
 
-class CustomStack {
+class CustomStack1 {
 
     Stack<Integer> stack;
+    int maxSize;
+    int currentSize;
 
 
-    public CustomStack(int maxSize) {
+    public CustomStack1(int maxSize) {
+        stack = new Stack<>();
+        this.maxSize = maxSize;
+        this.currentSize = 0;
     }
 
     public void push(int x) {
+        if (currentSize < maxSize) {
+            stack.push(x);
+            currentSize++;
+        }
 
     }
 
     public int pop() {
-
+        if (stack.isEmpty()) {
+            return -1;
+        }
+        currentSize--;
+        return stack.pop();
     }
 
     public void increment(int k, int val) {
 
+        List<Integer> l1 = new ArrayList<>();
+        while (!stack.isEmpty()) {
+            l1.add(0, stack.pop());
+        }
+
+        if (stack.size() <= k) {
+            l1 = l1.stream().map(x -> x + val).collect(Collectors.toList());
+        } else {
+
+            List<Integer> finalL = l1;
+            l1.replaceAll(i -> finalL.indexOf(i) < k ? i + 10 : i);
+        }
+
+        l1.forEach(x -> {
+            stack.push(x);
+        });
     }
 }
