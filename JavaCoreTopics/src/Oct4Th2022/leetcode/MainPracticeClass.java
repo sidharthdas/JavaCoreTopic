dc66cc146b83dba03a410243923c951c5dde4a26
sidharthdas <sidharthdas.1995@gmail.com>
2023-03-30 15:15:59 +0530

    Leetcode

--- JavaCoreTopics/src/Oct4Th2022/leetcode/MainPracticeClass.java
+++ JavaCoreTopics/src/Oct4Th2022/leetcode/MainPracticeClass.java
@@ -23,6 +23,7 @@ public class MainPracticeClass {
         //System.out.println(countPairs(new int[]{2160, 1936, 3, 29, 27, 5, 2503, 1593, 2, 0, 16, 0, 3860, 28908, 6, 2, 15, 49, 6246, 1946, 23, 105, 7996, 196, 0, 2, 55, 457, 5, 3, 924, 7268, 16, 48, 4, 0, 12, 116, 2628, 1468}));
         //System.out.println(findMinDifference(Arrays.asList("05:31", "22:08", "00:35")));
         System.out.println(findLHS(new int[]{1, 3, 2, 2, 5, 2, 3, 7}));
+
     }
 
     public int largestSumAfterKNegations(int[] nums, int k) {
