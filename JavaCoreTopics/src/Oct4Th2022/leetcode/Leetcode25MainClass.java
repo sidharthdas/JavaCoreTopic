2259e5b26c79965306f5864c9aa80438a815b3bf
sidharthdas <sidharthdas.1995@gmail.com>
2023-06-05 22:34:07 +0530

    Leetcode

--- JavaCoreTopics/src/Oct4Th2022/leetcode/Leetcode25MainClass.java
+++ JavaCoreTopics/src/Oct4Th2022/leetcode/Leetcode25MainClass.java
@@ -1519,7 +1519,6 @@ public class Leetcode25MainClass {
                 l1.add(citations[i]);
             }
         }
-
         return l1.stream().sorted(Comparator.reverseOrder()).findFirst().get();
 
     }
