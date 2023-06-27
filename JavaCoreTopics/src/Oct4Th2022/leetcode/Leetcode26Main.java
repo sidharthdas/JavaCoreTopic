4a5220d865ec5b2e68260164509be84f09d61094
sidharthdas <sidharthdas.1995@gmail.com>
2023-06-27 18:49:28 +0530

    Leetcode

--- JavaCoreTopics/src/Oct4Th2022/leetcode/Leetcode26Main.java
+++ JavaCoreTopics/src/Oct4Th2022/leetcode/Leetcode26Main.java
@@ -25,7 +25,27 @@ public class Leetcode26Main {
         //makeSmallestPalindrome("egcfe");
         //checkStraightLine(new int[][]{new int[]{0, 0}, new int[]{0, 1}, new int[]{0, -1}});
         //System.out.println(totalMoney(10));
-        maxSubsequence(new int[]{2, 1, 3, 3}, 2);
+        //maxSubsequence(new int[]{2, 1, 3, 3}, 2);
+        findOriginalArray(new int[]{1,3,4,2,6,8});
+    }
+
+    public static  int[] findOriginalArray(int[] changed) {
+        int len = changed.length;
+        List<Integer> l = new ArrayList<>();
+        List<Integer> changeList = Arrays.stream(changed).boxed().collect(Collectors.toList());
+
+        for(int i : changed){
+            if(changeList.contains(i*2)){
+                changeList.remove(new Integer(i));
+                changeList.remove(new Integer(i*2));
+                l.add(i);
+            }
+        }
+
+        if(changeList.isEmpty()){
+            return l.stream().mapToInt(x -> x).toArray();
+        }
+        return new int[0];
     }
 
     public static int[] maxSubsequence(int[] nums, int k) {
