42e3fe19a5ae4eba39ef67d7c67387973cb931a4
sidharthdas <sidharthdas.1995@gmail.com>
2023-07-15 00:40:17 +0530

    Leetcode

--- JavaCoreTopics/src/Oct4Th2022/leetcode/Leetcode25MainClass.java
+++ JavaCoreTopics/src/Oct4Th2022/leetcode/Leetcode25MainClass.java
@@ -102,10 +102,10 @@ public class Leetcode25MainClass {
     public int rotatedDigits(int n) {
         int count = 0;
 
-        for(int i = 1; i <= n; i++){
-            String temp = String.valueOf(i) ;
-            if(temp.contains("2") || temp.contains("5") || temp.contains("6") || temp.contains("9")) {
-                if(!temp.contains("3") && !temp.contains("7") && !temp.contains("4")){
+        for (int i = 1; i <= n; i++) {
+            String temp = String.valueOf(i);
+            if (temp.contains("2") || temp.contains("5") || temp.contains("6") || temp.contains("9")) {
+                if (!temp.contains("3") && !temp.contains("7") && !temp.contains("4")) {
                     count++;
                 }
             }
