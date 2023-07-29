b3adfccf32992f200337761c1ded26806c52d04c
sidharthdas <sidharthdas.1995@gmail.com>
2023-07-29 23:21:56 +0530

    Leetcode

---
+++ JavaCoreTopics/src/Oct4Th2022/leetcode/Leetcode26MainClass.java
@@ -0,0 +1,29 @@
+package Oct4Th2022.leetcode;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Leetcode26MainClass {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public int maxCount(int[] banned, int n, int maxSum) {
+        List<Integer> l = Arrays.stream(banned).boxed().toList();
+        int count = 0;
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if(!l.contains(i)){
+                if(sum+ i <= maxSum){
+                    sum += i;
+                    count++;
+                } else {
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+}
