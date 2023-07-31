abeb12679f19a4401a6298ce0a4aedfe8a253d28
sidharthdas <sidharthdas.1995@gmail.com>
2023-07-31 11:00:02 +0530

    Leetcode

--- JavaCoreTopics/src/Oct4Th2022/leetcode/Leetcode26MainClass.java
+++ JavaCoreTopics/src/Oct4Th2022/leetcode/Leetcode26MainClass.java
@@ -1,11 +1,52 @@
 package Oct4Th2022.leetcode;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Leetcode26MainClass {
 
     public static void main(String[] args) {
+    }
+
+    public String sortVowels(String s) {
+        String[] srr = s.split("");
+
+        int len = srr.length;
+
+        List<String> caps = new ArrayList<>();
+        List<String> small = new ArrayList<>();
+
+        for (int i = 0; i < len; i++) {
+            if (List.of("a", "e", "i", "o", "u").contains(srr[i])) {
+                small.add(srr[i]);
+                srr[i] = "#00#";
+            } else if (List.of("A", "E", "I", "O", "U").contains(srr[i])) {
+                caps.add(srr[i]);
+                srr[i] = "#00#";
+            }
+        }
+
+        Collections.sort(caps);
+        Collections.sort(small);
+        caps.addAll(small);
+
+        int index = 0;
+        for (int i = 0; i < len; i++) {
+            if (srr[i].equals("#00#")) {
+                srr[i] = caps.get(index);
+                index++;
+            }
+        }
+
+        String temp = "";
+
+        for (String s1 : srr) {
+            temp += s1;
+        }
+
+        return temp;
     }
 
     public int maxCount(int[] banned, int n, int maxSum) {
